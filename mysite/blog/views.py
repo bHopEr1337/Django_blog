3ec43190dac7efc2af3d67864ac692c81b6b50dd
Alex <pkuslin9@gmail.com>
2024-07-13 17:31:11 +0300

    start

---
+++ mysite/blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  context={'posts':posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+              'blog/post/detail.html',
+              context={'post':post})
