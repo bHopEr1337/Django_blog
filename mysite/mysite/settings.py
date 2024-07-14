3932c13cffb69fa4f8bc453f0546f0b55f30d067
Alex <pkuslin9@gmail.com>
2024-07-14 12:56:30 +0300

    email sent serv

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pkuslin9@gmail.com'
+EMAIL_HOST_PASSWORD = 'rbeo zabk ijep whee'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
