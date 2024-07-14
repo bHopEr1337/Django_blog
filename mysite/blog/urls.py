afebacf3c8c3340b2c0a5812a9e90d062e4e1f9a
Alex <pkuslin9@gmail.com>
2024-07-14 18:13:02 +0300

    Comment system

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
          views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/',
-         views.post_share, name='post_share')
+         views.post_share, name='post_share'),
+    path('<int:post_id>/comment/',
+         views.post_comment, name='post_comment'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 class PostListView(ListView):
     """
@@ -37,9 +38,13 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                          'comments': comments,
+                          'form': form})
 
 
 def post_share(request, post_id):
@@ -66,3 +71,23 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post':post,
                                                                             'form':form,
                                                                             'sent':sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
