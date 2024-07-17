21ef5e9c08eacb557f30d781501cea1ab15e8c80
Alex <pkuslin9@gmail.com>
2024-07-17 11:52:25 +0300

    adding sitemap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
 
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
