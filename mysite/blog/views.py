0e637e09c37da8fdcbf2782d714aec18b839d6ed
Alex <pkuslin9@gmail.com>
2024-07-17 22:26:28 +0300

    upgrade search system, adding rank and russian dict

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 
 class PostListView(ListView):
@@ -113,17 +113,19 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
-
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
-
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
     return render(request,
                   'blog/post/search.html',
-                  {'form':form,
-                   'query':query,
-                   'results':results})
+                  {'form': form,
+                   'query': query,
+                   'results': results})
